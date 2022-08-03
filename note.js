b60703d8cdbb135e403743f9034950dde2de47a4
Azimul767 <75627038+Azimul767@users.noreply.github.com>
2022-08-03 20:14:09 +0600

    Add files via upload

---
+++ note.js
@@ -0,0 +1,50 @@
+var price = 66;
+var age = 55;
+var total= price+age;
+console.log (total)
+// (55) this is oparet (=,-,*,/ ) oparetor (20+30= output) oparetion
+
+// string to number 
+var price = parseInt;
+var price = parseFloat
+
+
+// shorthand example
+var price=31;
+var price = price+20;
+or
+var price += 20;
+
+// type dekhar jonn0
+var price = 30;
+console.log(typeof price);
+
+// true or false egulo holo boolean type 
+
+var num1= 0.1;
+var num2 = 0.2;
+var sum = num1+num2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+console.log(sum)
+
+var mango=7;
+var people =2;
+
+console.log (mango%people)
+
+// /this mean vagfol andm   % this means vag shes
+
+// array 
+var name = [ "sheblu", "tuhin", "ramim", "Shafin"]
+console.log(name)
+
+// Mot Kotogula ache ta dekhar jonno
+
+console.log (name.length);
+
+// Find Elements bu index
+console.log(name[0]);
+
+var position = name.indexOf("tuhin");
+console.log(position)
